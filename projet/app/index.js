e7506236cb6f7324a1a2ca2c967739c146f0f138
Ahmed Gacem <gacema426@gmail.com>
2025-12-11 17:00:08 -0500

    Add : recherche page with flatlist (incomplet)

--- projet/app/index.js
+++ projet/app/index.js
@@ -1,18 +1,16 @@
 import { router } from "expo-router";
-import { ImageBackground, Pressable, StyleSheet, Text, View } from "react-native";
+import { ImageBackground, Pressable, StyleSheet, Text } from "react-native";
 
 const imageLachute = require("../assets/images/lachute.jpg");
 
 export default function Acceuil() {
   return (
-    <View >
-        <ImageBackground source={imageLachute} style={styles.image}>
-          <Text style={styles.title}>La chute</Text>
-          <Pressable onPress={() => router.push("/recherche")}>
-            <Text style={styles.button}>Recherche</Text>
-          </Pressable>
-        </ImageBackground>
-    </View>
+      <ImageBackground source={imageLachute} style={styles.image}>
+        <Text style={styles.title}>La chute</Text>
+        <Pressable onPress={() => router.push("/recherche")}>
+          <Text style={styles.button}>Recherche</Text>
+        </Pressable>
+      </ImageBackground>
   );
 }
 
@@ -21,24 +19,24 @@ const styles = StyleSheet.create(
     image :{
       flex:1,
       alignItems:"center",
-      justifyContent:"space-around"
+      justifyContent:"center",
+
     },
 
     title : {
       fontSize : 80,
       fontStyle : "italic",
-      textAlign : "center"
-      
+      textAlign : "center",
+      height : "60%"
     },
 
     button :{
       padding : 10,
-      margin : "auto",
       fontSize : 20,
       backgroundColor : "lightgreen",
       borderRadius : 10,
-      textAlign : "center"
-      
+      textAlign : "center",
+
     }
 
   }
--- projet/app/recherche.js
+++ projet/app/recherche.js
@@ -1,9 +1,62 @@
-import { Text, View } from "react-native";
+// import * as Location from 'expo-location';
+import { useState } from "react";
+import { FlatList, StyleSheet, Text, View } from "react-native";
+import { commerces } from "../assets/libs/donnees";
 
 export default function Recherche() {
+
+  // const [location, setLocation] = useState<Location.LocationObject | null>(null);
+  // const [errorMsg, setErrorMsg] = useState<string | null>(null);
+  const [donnees, setDonnees] = useState(commerces)
+
+  //   useEffect(() => {
+  //   async function getCurrentLocation() {
+      
+  //     let { status } = await Location.requestForegroundPermissionsAsync();
+  //     if (status !== 'granted') {
+  //       setErrorMsg('Permission to access location was denied');
+  //       return;
+  //     }
+
+  //     let location = await Location.getCurrentPositionAsync({});
+  //     setLocation(location);
+  //   }
+
+  //   getCurrentLocation();
+  // }, []);
+
+  // let text = 'Waiting...';
+  // if (errorMsg) {
+  //   text = errorMsg;
+  // } else if (location) {
+  //   text = JSON.stringify(location);
+  // }
+
   return (
     <View>
-      <Text>recherche</Text>
+      {/* <Text>{text}</Text> */}
+
+      <FlatList data={commerces} renderItem={({item}) =>
+        <View style={styles.commerce}>
+          <Text>{item.nom}</Text>
+          <Text>latitude : {item.latitude}</Text>
+          <Text>longitude : {item.longitude}</Text>
+        </View>
+      }/>
+
     </View>
   );
 }
+
+const styles = StyleSheet.create(
+  {
+    commerce :{
+      backgroundColor : "lightgreen",
+      margin : 10,
+      padding : 10,
+      borderRadius : 10,
+    },
+
+  }
+)
+
